75c51335454654835ebf4ccbe89c97ca65e14370
Dagmawi <dagmawis603@gmail.com>
2021-01-29 06:01:54 +0300

    minor modification

---
+++ slider.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".questions-container")
+const slide =document.querySelectorAll(".question")
+
+// buttons 
+const next = document.getElementById("next__btn")
+
+//counter
+let counter = 1
+const size = slide[0].clientWidth;
+
